bc6cd61406a6335ec045fb1dff6605b589570e34
Nicole Analise Cox <nicoleanalisecox@gmail.com>
2021-08-15 17:00:04 +0100

    install axios, use with async await to get general data from Unsplash API

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import config from '../config';
 import SearchForm from './SearchForm';
 import ImageList from './ImageList';
+import axios from 'axios';
 
-// const accessKey = config.access_key;
+const accessKey = config.access_key;
 
 const App = () => {
     
-    const onFormSubmit = () => {
-        console.log("Get images from API")
+    const onFormSubmit = async (term: string) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', 
+        {
+            params: { query: term },
+            headers: {
+                Authorization: `Client-ID ${accessKey}`
+            }
+        }
+        )
+
+        console.log(response);
 }
 
 
